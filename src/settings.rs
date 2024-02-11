764486a54881f125797a67d34b2daff17c41de07
Paul Johannes Wagner <hydrofin@gmail.com>
2024-02-11 17:33:57 +0100

    remove the RUN_MODE from the settings
    They are not really used anyway. Defaults and Local with env variables should be sufficient. Additional complexity is unnecessary.

--- src/settings.rs
+++ src/settings.rs
@@ -43,23 +43,20 @@ pub struct Settings {
 }
 
 impl Settings {
+    /// Loads and creates a new instance of the application settings.
+    /// The settings are composed of the `config/default`, the `config/local`, and the environment variables.
     pub fn new() -> Result<Self, ConfigError> {
-        let run_mode = env::var("RUN_MODE").unwrap_or_else(|_| "development".into());
         let env_prefix = env::var("ENV_PREFIX").unwrap_or_else(|_| "xenos".into());
 
         let s = Config::builder()
             // Start off by merging in the "default" configuration file
             .add_source(File::with_name("config/default"))
-            // Add in the current environment file
-            // Default to 'development' env
-            // Note that this file is _optional_
-            .add_source(File::with_name(&format!("config/{}", run_mode)).required(false))
             // Add in a local configuration file
             // This file shouldn't be checked in to git
             .add_source(File::with_name("config/local").required(false))
             // Add in settings from the environment (with a prefix of APP)
             // E.g. `APP__DEBUG=1` would set the `debug` key, on the other hand,
-            // `APP__REDIS_CACHE__ENABLED=0` would disable the redis cache.
+            // `XENOS__REDIS_CACHE__ENABLED=0` would disable the redis cache.
             .add_source(Environment::with_prefix(&env_prefix).separator("__"))
             .build()?;
 
