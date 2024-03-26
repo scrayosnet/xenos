f5f4dbfc87b4feaa9708d48912e890220e132de9
Paul Wagner <hydrofin@gmail.com>
2024-03-26 16:14:39 +0100

    add optional experimental flag to proto build

--- build.rs
+++ build.rs
@@ -1,5 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .build_client(false)
         .type_attribute(".", "#[derive(serde::Serialize,serde::Deserialize)]")
         .compile(&["proto/profile.proto"], &["proto"])?;
